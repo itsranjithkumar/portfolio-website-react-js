42ce51fb45733f98014010f82f60cfd7361fba09
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-07-15 16:46:57 +0530

    error

---
+++ ranjith_portfolio/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+
+const App = () => {
+  return (
+    <>
+    
+    <Navbar/>
+    <div className='container'>
+      <Hero/>
+      </div>
+
+      
+    </>
+  );
+};
+
+export default App
---
+++ ranjith_portfolio/src/components/Navbar/MobileNav/MobileNav.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import'./MobileNav.css'
+import logo from '../../../assets/mylogo.png'
+
+const MobileNav = ({isOpen, toggleMenu}) => {
+    return (
+      <>
+      <div
+          className={`Mobile-menu} ${isOpen ? "active": ""}`}
+          onClick={toggleMenu}
+         >
+
+      <div className="mobile-menu-container">
+        <img src={logo} alt='' className='logo' />
+        <ul>
+          <li>
+            <a className='menu-item'>Home</a>
+          </li>
+
+          <li>
+                <a className='menu-item'>Skills</a>
+                </li>
+                <li>
+                <a className='menu-item'>Work Experience</a>
+                </li>
+                <li>
+                <a className='menu-item'>contact Me</a>
+                </li>
+
+                <button className="contact-btn" onClick={()=> {}}>
+                Hire Me
+                </button>
+        </ul>
+
+        </div>
+      </div>
+</>
+  )
+}
+export default MobileNav;
