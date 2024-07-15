4a64be98793587f17ed93d1487c5a16ed7256a27
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-07-15 18:03:29 +0530

    24

--- ranjith_portfolio/src/components/Hero/Hero.jsx
+++ ranjith_portfolio/src/components/Hero/Hero.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
-import './Hero.css'
+import React from "react";
+import "./Hero.css";
+
+import myimage from "../../assets/myimage.jpg";
+import htmlimage from "../../assets/htmlimage.png";
+import imagejs from "../../assets/imagejs.png";
+import imagecss from "../../assets/imagecss.png";
 
 const Hero = () => {
   return (
-    <div>
-      Hero
-    </div>
-  )
-}
+    <section className="hero-container">
+      <div className="hero-content">
+        <h2>Building Digital Experiences That Inspire</h2>
+        <p>
+          Passionate Frontened Developper Transforming Ideas into seanless and
+          visually Stunning web Solution
+        </p>
+      </div>
 
-export default Hero
+      <div className="hero-img">
+        <div>
+          <div className="tech-icon">
+            <img src={myimage} alt="" />
+          </div>
+          <img src={htmlimage} alt="" />
+        </div>
+
+        <div>
+          <div className="tech-icon">
+            <img src={imagejs} alt="" />
+        </div>
+        <div className="tech-icon">
+          <img src={imagecss} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
