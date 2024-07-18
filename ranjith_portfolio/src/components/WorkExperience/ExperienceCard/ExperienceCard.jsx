47caf9862aa893f6ac89075c2dffa1c431880151
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-07-18 18:03:50 +0530

    58:28

--- ranjith_portfolio/src/components/WorkExperience/ExperienceCard/ExperienceCard.jsx
+++ ranjith_portfolio/src/components/WorkExperience/ExperienceCard/ExperienceCard.jsx
@@ -2,10 +2,20 @@ import React from 'react'
 import './ExperienceCard.css'
 const ExperienceCard = ({details}) => {
   return (
-    <div>
-      ExperienceCard
+    <div className='work-experience-card'>
+        <h5>{details.title}</h5>
+        <div className='work-duration'>{details.data}</div>
+
+    <ul>
+        {details.responsibilities.map((item)=> (
+            <li key={item}>{item}</li>
+        ))}
+        </ul>    
     </div>
-  )
+    )
 }
+        
+        
+        
 
 export default ExperienceCard
--- ranjith_portfolio/src/components/WorkExperience/WorkExperience.jsx
+++ ranjith_portfolio/src/components/WorkExperience/WorkExperience.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import './WorkExperience.css'
+import { WorkExperience } from '../../../utils/data'
+import ExperienceCard from './ExperienceCard/ExperienceCard'
 
 const WorkExperience = () => {
   return (
     <section className='experience-container'>
         <h5>Work Experience</h5>
 
-      <div className='experience-content'></div>
-    </section>
-  )
-}
+      <div className='experience-content'>
 
-export default WorkExperience
+        {WorkExperience.map((item)=> (
+            
+            <ExperienceCard key={item.title} details={item}/>
+        ))}
+    </div>
+    </section>
+  );
+};
+
+export default WorkExperience;WorkExperience
