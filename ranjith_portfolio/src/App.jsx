f6ddd0c1267cb90e1b0fc04c549a6f0e54a5f34f
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-08-20 14:15:17 +0530

    error fixed

--- ranjith_portfolio/src/App.jsx
+++ ranjith_portfolio/src/App.jsx
@@ -1,27 +1,25 @@
 import React from 'react'
-import Navbar from './components/Navbar/Navbar';
-import Hero from './components/Hero/Hero';
-import Skills from './components/Skills/Skills';
-import WorkExperience from './components/WorkExperience/WorkExperience';
-import ContactMe from './components/ContactMe/ContactMe';
-import Footer from './components/Footer/Footer';
-
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import './App.css'
+import Skills from './components/Skills/Skills'
+import WorkExperience from './components/WorkExperience/WorkExperience'
+import ContactMe from './components/ContactMe/ContactMe'
+import Footer from './components/Footer/Footer'
 const App = () => {
   return (
     <>
-    
-    <Navbar/>
-    <div className='container'>
-      <Hero/>
-      <Skills/>
-      <WorkExperience/>
-      <ContactMe/>
-      </div>
-      <Footer/>
+    <Navbar />
+    <div className="container">
 
-      
+    <Hero/>
+    <Skills/>
+    <WorkExperience/>
+    <ContactMe/>
+    </div>
+    <Footer/>
     </>
-  );
-};
+  )
+}
 
 export default App
--- ranjith_portfolio/src/components/Hero/Hero.jsx
+++ ranjith_portfolio/src/components/Hero/Hero.jsx
@@ -1,41 +1,47 @@
-import React from "react";
-import "./Hero.css";
+import React from 'react'
 
-import myimage from "../../assets/myimage.jpg";
-import htmlimage from "../../assets/htmlimage.png";
-import imagejs from "../../assets/imagejs.png";
-import imagecss from "../../assets/imagecss.png";
+import './Hero.css'
+import backend_img from '../../assets/icons8-backend-development-64.png'
+import gokul_img from '../../assets/gokul.jpeg'
+import html_img from '../../assets/icons8-html5-144.png'
+import css_img from '../../assets/icons8-css-144.png'
+import js_img from '../../assets/icons8-js-144.png'
+
+
+
 
 const Hero = () => {
   return (
     <section className="hero-container">
-      <div className="hero-content">
+        <div className="hero-content">
+
         <h2>Building Digital Experiences That Inspire</h2>
         <p>
           Passionate Frontened Developper Transforming Ideas into seanless and
           visually Stunning web Solution
         </p>
-      </div>
-
-      <div className="hero-img">
-        <div>
-          <div className="tech-icon">
-            <img src={myimage} alt="" />
-          </div>
-          <img src={htmlimage} alt="" />
         </div>
-
-        <div>
-          <div className="tech-icon">
-            <img src={imagejs} alt="" />
+        <div className="hero-img">
+            <div>
+                <div className="tech-icon">
+                <img src={backend_img} alt="react" />
+                </div>
+                <img  className="hero_main_image"src={gokul_img} alt="gokul" />
+            </div>
+            <div>
+                <div className="tech-icon">
+                <img src={html_img} alt="html" />
+                </div>
+                 <div className="tech-icon">
+                <img src={css_img} alt="css" />
+                </div>
+                 <div className="tech-icon">
+                <img src={js_img} alt="js" />
+                </div>
+            </div>
         </div>
-        <div className="tech-icon">
-          <img src={imagecss} alt="" />
-          </div>
-        </div>
-      </div>
     </section>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
