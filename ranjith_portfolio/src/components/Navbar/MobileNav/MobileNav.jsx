5005941ac9869faf2ff88eb98e7a72faccb569b5
itsranjithkumar <ranjithkumarms28@gmail.com>
2024-09-12 21:49:49 +0530

    mobile nav bug fixed

--- ranjith_portfolio/src/components/Navbar/MobileNav/MobileNav.jsx
+++ ranjith_portfolio/src/components/Navbar/MobileNav/MobileNav.jsx
@@ -1,40 +1,35 @@
-import React from 'react'
-import'./MobileNav.css'
-import logo from '../../../../public/assets/mylogo.png'
+import React from 'react';
+import './MobileNav.css';
+import logo from '../../../../public/assets/mylogo.png';
 
-const MobileNav = ({isOpen, toggleMenu}) => {
-    return (
-      <>
-      <div
-          className={`mobile-menu ${isOpen ? "active": ""}`}
-          onClick={toggleMenu}
-         >
-
+const MobileNav = ({ isOpen, toggleMenu }) => {
+  return (
+    <div
+      className={`mobile-menu ${isOpen ? "active" : ""}`}
+      onClick={toggleMenu}
+    >
       <div className="mobile-menu-container">
-        <img src={logo} alt='' className='logo' />
+        <img src={logo} alt='Logo' className='logo' />
         <ul>
           <li>
-            <a className='menu-item'>Home</a>
+            <a className='menu-item' href="#home" onClick={toggleMenu}>Home</a>
           </li>
-
           <li>
-                <a className='menu-item'>Skills</a>
-                </li>
-                <li>
-                <a className='menu-item'>Work Experience</a>
-                </li>
-                <li>
-                <a className='menu-item'>contact Me</a>
-                </li>
-
-                <button className="contact-btn" onClick={()=> {}}>
-                Hire Me
-                </button>
+            <a className='menu-item' href="#skills" onClick={toggleMenu}>Skills</a>
+          </li>
+          <li>
+            <a className='menu-item' href="#work-experience" onClick={toggleMenu}>Work Experience</a>
+          </li>
+          <li>
+            <a className='menu-item' href="#contact-me" onClick={toggleMenu}>Contact Me</a>
+          </li>
+          <button className="contact-btn" onClick={() => { /* Your logic here */ }}>
+            Hire Me
+          </button>
         </ul>
-
-        </div>
       </div>
-</>
-  )
-}
+    </div>
+  );
+};
+
 export default MobileNav;
