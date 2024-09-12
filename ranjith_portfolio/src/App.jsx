3a246e06abe5f316837f2e716a96bd507e264b78
itsranjithkumar <ranjithkumarms28@gmail.com>
2024-09-12 17:34:59 +0530

    menu bug fixed

--- ranjith_portfolio/src/App.jsx
+++ ranjith_portfolio/src/App.jsx
@@ -14,6 +14,7 @@ import GithubStreak from './components/GithubStreak/GithubStreak'
 
 const App = () => {
   return (
+    
     <>
     <Navbar />
     <div className="container" id="home">
--- ranjith_portfolio/src/components/Navbar/Navbar.jsx
+++ ranjith_portfolio/src/components/Navbar/Navbar.jsx
@@ -2,51 +2,38 @@ import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../../../public/assets/mylogo.png';
 import MobileNav from './MobileNav/MobileNav';
+
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
 
   const toggleMenu = () => {
     setOpenMenu(!openMenu);
   };
+
   return (
     <>
-    <MobileNav isOpen={openMenu} toggleMenu={toggleMenu}/>
+      {/* Mobile navigation */}
+      <MobileNav isOpen={openMenu} toggleMenu={toggleMenu} />
 
       <nav className='nav-wrapper'>
         <div className='nav-content'>
-            <img className='logo' src={logo} alt=''/>
-            <ul>
-                <li>
-                <a href='#home' className='menu-item'>Home</a>
-                </li>
-                <li>
-                <a href='#skills' className='menu-item'>Skills</a>
-                </li>
-                <li>
-                <a href='#Work_experience' className='menu-item'>Work Experience</a>
-                </li>
-                <li>
-                <a href='#contact_Me' className='menu-item'>contact Me</a>
-                </li>
+          <img className='logo' src={logo} alt='Logo' />
 
-                <button className='contact-btn' onClick={() => {}}>
-                    Hire me
+          {/* Desktop menu */}
+          <ul className='desktop-menu'>
+            <li><a href="#home" className="menu-item">Home</a></li>
+            <li><a href="#skills" className="menu-item">Skills</a></li>
+            <li><a href="#Work_experience" className="menu-item">Work Experience</a></li>
+            <li><a href="#contact_Me" className="menu-item">Contact Me</a></li>
+            <button className="contact-btn">Hire Me</button>
+          </ul>
 
-                </button>
-            </ul>
-            <button className="menu-btn" onClick={toggleMenu}>
-                <span
-                  className={'material-symbols-outlined'}
-                  style={{fontSize: '1.8rem'}}
-
-                >
-                    {openMenu ? 'close' : 'menu'}
-
-                </span>
-
-            </button>
+          {/* Menu button for mobile */}
+          <button className="menu-btn" onClick={toggleMenu} aria-label="Toggle menu">
+            <i className={`fas ${openMenu ? 'fa-times' : 'fa-bars'}`} style={{ fontSize: '1.8rem' }}></i>
+          </button>
         </div>
-        </nav>
+      </nav>
     </>
   );
 };
