0779a00eca11a7f1efd25774fab50e4cc8e54ed1
itsranjithkumar <163500531+itsranjithkumar@users.noreply.github.com>
2024-09-19 23:10:43 +0530

    project added

--- ranjith_portfolio/src/components/Projects/Projects.jsx
+++ ranjith_portfolio/src/components/Projects/Projects.jsx
@@ -1,27 +1,30 @@
-// Projects.jsx
 import React from 'react';
 import './Projects.css'; // Importing the CSS file
 
 const projectsData = [
   {
     title: 'Python API Project',
-    image: 'path/to/python-api-project.jpg', // Update with the correct path
-    description: 'A Python-based REST API project that handles user data with CRUD operations.',
+    image: 'public/assets/FastAPI_b.png', // Update with the correct path
+    description: 'Created complete Backend API’s and unit testing for a social media platform using FastAPI, Docker, PostgreSQL, Postman, JWT authentication, etc.',
+    link: 'https://github.com/itsranjithkumar/python-api'
   },
   {
-    title: 'Project 2',
-    image: 'path/to/project2-image.jpg',
-    description: 'This is a brief description of Project 2.',
+    title: 'React-login validation',
+    image: 'public/assets/login.png',
+    description: 'Developed using React for a dynamic UI, React Hook Form for efficient form handling, Yup for validation, and styled using CSS.',
+    link: 'https://github.com/itsranjithkumar/react-js-from-validation/tree/main/app'
   },
   {
-    title: 'E-commerce Website',
-    image: 'path/to/ecommerce-website.jpg', // Add the path to the new project image
-    description: 'An e-commerce website built using React and Node.js, featuring a shopping cart, payment gateway, and product search.',
+    title: 'React-Fitness-website',
+    image: 'public/assets/fitness2.png', // Add the path to the new project image
+    description: 'Designed and developed a gym application using React and JavaScript. This website consists of two pages: Home and Exercises. Built and deployed on Netlify.',
+    link: 'https://github.com/itsranjithkumar/React-Fitness-app-'
   },
   {
-    title: 'Portfolio Website',
-    image: 'path/to/portfolio-website.jpg', // Add the path to the new project image
-    description: 'A personal portfolio website showcasing my skills, projects, and contact information. Built using React and styled-components.',
+    title: 'React-Amazon clone',
+    image: 'public/assets/amazon.png', // Add the path to the new project image
+    description: 'Developed a fully functional Amazon clone using React for building the user interface, creating reusable components like product listings, shopping cart, and checkout.',
+    link: 'https://github.com/itsranjithkumar/REACT-NATIVE'
   },
 ];
 
@@ -31,15 +34,17 @@ const Projects = () => {
       <h2 className="projects-heading">My Projects</h2> {/* Project heading */}
       <div className="projects-grid-container">
         {projectsData.map((project, index) => (
-          <div className="project-card" key={index}>
-            <div className="project-image-container">
-              <img src={project.image} alt={`${project.title} front page`} className="project-image" />
-              <div className="overlay">
-                <h3 className="project-title">{project.title}</h3>
-                <p className="project-description">{project.description}</p>
+          <a href={project.link} target="_blank" rel="noopener noreferrer" className="project-card-link" key={index}> {/* Moved the anchor to wrap the card */}
+            <div className="project-card">
+              <div className="project-image-container">
+                <img src={project.image} alt={`${project.title} front page`} className="project-image" />
+                <div className="overlay">
+                  <h3 className="project-title">{project.title}</h3>
+                  <p className="project-description">{project.description}</p>
+                </div>
               </div>
             </div>
-          </div>
+          </a>
         ))}
       </div>
     </div>
